0bf4697be8c8c4f2a56f7b02c12b0992ddae04ee
trungnghia2009 <trantrungnghia0603@gmail.com>
2018-12-05 00:30:49 +0700

    finish blog detail

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -8,6 +8,17 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        if len(self.body) > 40:
+            return self.body[:40] + '...'
+        else:
+            return self.body[:40]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 
 
 
